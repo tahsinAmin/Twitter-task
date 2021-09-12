58840d89bad69cf8193543c39deda5610be2d66a
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-09-12 17:43:22 +0600

    Sidebar done.

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,28 @@
+import { FiTwitter }  from 'react-icons/fi'
+import { HiOutlineUserCircle }  from 'react-icons/hi'
+import { BellIcon, MailIcon, UserIcon, HomeIcon, DotsHorizontalIcon, UserCircleIcon } from '@heroicons/react/solid'
+import { FlagIcon, PlayIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline'
+
 function Sidebar() {
    return (
-      // <div className='float-left border hidden md:flex md:w-1/12 h-screen bg-white'>
-      <div className='hidden float-left md:flex md:w-1/12 border h-screen flex-col justify-between py-6'>
-         <div className="s">Logo and icons</div>
-         <div className="s">Profile</div>
+      <div className='hidden float-left  md:w-1/12 border h-screen md:flex flex-col justify-between py-3 items-center'>
+         <div className="s">
+            <div className="flex flex-col items-center space-y-4">
+               <div className="flex flex-col items-center space-y-8">
+                  <FiTwitter className='mb-2' size='2.5em'/>
+                  <HomeIcon/>
+                  <SearchIcon/>
+                  <BellIcon/>
+                  <MailIcon/>
+                  <UserIcon/>
+                  <DotsHorizontalIcon/>
+
+               </div>
+               <div className="rounded-full h-14 w-14 bg-blue-400 "></div>
+            </div>
+         </div>
+         <HiOutlineUserCircle size='4.5em'/>
+         {/* <UserCircleIcon/> */}
       </div>
    )
 }
