e4161e41af5a09e1504f1aa8953f73096336bef4
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-09-12 08:56:57 +0600

    Added data. Mobile done as a draft

--- components/Content.js
+++ components/Content.js
@@ -1,10 +1,59 @@
 import { GoVerified } from 'react-icons/go'
 import { FiMoreHorizontal, FiMessageCircle, FiRepeat,FiHeart, FiUpload } from "react-icons/fi";
 
+const data = [
+   {
+      id:1,
+      name:"Kevin Hart",
+      username:"@KevinHart4Real",
+      verified:true,
+      time: "12h",
+      msgBody: "Just posted a photo https://youtu.be/9jX?sasdzczx/...",
+      msg:"3.2K",
+      repeat:"3.2K",
+      heart:"3.2K",
+   },
+   {
+      id:2,
+      name:"Kevin Hart",
+      username:"@KevinHart4Real",
+      verified:true,
+      time: "12h",
+      msgBody: "Just posted a photo https://youtu.be/9jX?sasdzczx/...",
+      msg:"3.2K",
+      repeat:"3.2K",
+      heart:"3.2K",
+   },
+   {
+      id:3,
+      name:"Kevin Hart",
+      username:"@KevinHart4Real",
+      verified:true,
+      time: "12h",
+      msgBody: "Just posted a photo https://youtu.be/9jX?sasdzczx/...",
+      msg:"3.2K",
+      repeat:"3.2K",
+      heart:"3.2K",
+   },
+   {
+      id:4,
+      name:"Kevin Hart",
+      username:"@KevinHart4Real",
+      verified:true,
+      time: "12h",
+      msgBody: "Just posted a photo https://youtu.be/9jX?sasdzczx/...",
+      msg:"3.2K",
+      repeat:"3.2K",
+      heart:"3.2K",
+   },
+]
+
 function Content() {
+   // console.log(data);
    return (
       <div className="w-screen">
-         <div className=" h-40 flex justify-between border border-t-1 border-b-1">
+         {data.map(({id, name, username, verified, time, msgBody, msg, repeat, heart}) => (
+            <div key={id} className="h-40 flex justify-between border border-t-1 border-b-1">
             <div className="h-auto w-1/5 py-4 flex justify-center">
                <div className="rounded-full h-14 w-14 flex items-center justify-center bg-gray-400 "></div>
             </div>
@@ -12,32 +61,31 @@ function Content() {
                <div>
                   <div className="mb-1 flex justify-between items-center">
                      <div className="flex items-center space-x-1 px-1">
-                        <h3 className='text-base font-bold text-gray-800 '>Kevin Hart</h3>
+                        <h3 className='text-base font-bold text-gray-800 '>{name}</h3>
                         <GoVerified color='blue'/>
-                        <h3 className='text-base font-normal text-gray-600'>@Kev... · 10hr</h3>
+                        <h3 className='text-base font-normal text-gray-600'>{username}</h3>
                      </div>
                      <FiMoreHorizontal color='gray'/>
                   </div>
                   <div className="body">
-                     Just posted a photo
-                     https://youtu.be/9jX?sasdzczx/
-                     ...
+                     {msgBody}
                   </div>
                </div>
                <div className='flex justify-between items-center'>
                   <div className="flex w-12 justify-between items-center">
-                     <FiMessageCircle/> 3.2K
+                     <FiMessageCircle/> {msg}
                   </div>
                   <div className="flex w-12 justify-between items-center">
-                     <FiRepeat/> 3.2K
+                     <FiRepeat/> {repeat}
                   </div>
                   <div className="flex w-12 justify-between items-center">
-                     <FiHeart/> 3.2K
+                     <FiHeart/> {heart}
                   </div>
                   <FiUpload/>
                </div>
             </div>
-      </div>
+         </div>
+         ))}
       </div>
    )
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
 import { FiMail,FiBell, FiSearch, FiHome } from "react-icons/fi";
 function Navbar() {
    return (
-      <div className='sticky bottom-0 z-10 flex justify-between h-12 p-2'>
+      <div className='sticky bottom-0 z-10 flex justify-between h-12 p-2 bg-white md:hidden'>
          <div className="q"><FiHome size='2em'/></div>
          <div className="q"><FiSearch size='2em'/></div>
          <div className="q"><FiBell size='2em'/></div>
